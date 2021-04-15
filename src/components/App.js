e72b60085a4d6162b25f4319c07665477f3575b5
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-15 06:42:12 +0530

    connect App to redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    words: state.words,
+  };
+}
+
+export default connect(mapStateToProps)(App);
