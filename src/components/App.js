bd96f172044f09c5c852391e6a1a691ac246d37e
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-15 08:06:14 +0530

    add and update material UI theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 import { fetchWords } from '../actions/words';
+
+//  update material ui themes with ThemeProvider
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#38b6ff',
+      contrastText: '#fff',
+    },
+  },
+});
 
 class App extends React.Component {
   componentDidMount() {
@@ -9,7 +20,15 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <div className="main">
+            <div className="list"></div>
+          </div>
+        </div>
+      </ThemeProvider>
+    );
   }
 }
 
