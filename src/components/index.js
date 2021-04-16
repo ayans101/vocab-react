ab86d5236dd342e6ea3003d9b5565e4f2d1af849
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-16 09:53:03 +0530

    add UI for Word component

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import Page404 from './Page404';
 import WordsList from './WordsList';
 import AddNew from './AddNew';
 import PopUp from './PopUp';
+import Word from './Word';
 
-export { App, Navbar, Home, Page404, WordsList, AddNew, PopUp };
+export { App, Navbar, Home, Page404, WordsList, AddNew, PopUp, Word };
